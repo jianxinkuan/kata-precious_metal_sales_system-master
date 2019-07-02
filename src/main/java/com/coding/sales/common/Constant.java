8c0160c6b5292975fd28ef5f78c0ad55bb0f0ca8
liugh888@qq.com <liugh888@qq.com>
2019-07-02 18:05:29 +0800

    常量类

---
+++ src/main/java/com/coding/sales/common/Constant.java
@@ -0,0 +1,24 @@
+package com.coding.sales.common;
+
+/**
+ * @program: kata-precious_metal_sales_system-master
+ * @description: 常量类
+ * @author: admin
+ * @create: 2019-07-02 17:46
+ **/
+public class Constant {
+
+    public static final String NINE_POINT = "9折券";
+
+    public static final String NINE_HALF_POINT = "95折券";
+
+    public static final String THREE_THOUSAND = "每满3000元减350";
+
+    public static final String TWO_THOUSAND = "每满2000元减30";
+
+    public static final String ONE_THOUSAND = "每满1000元减10";
+
+    public static final String THREE_HALF = "第3件半价";
+
+    public static final String THREE_ADD_ONE = "满3送1";
+}
